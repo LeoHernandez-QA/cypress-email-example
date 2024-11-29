46cd4b3fb1f519f73718eb0e69792d76452d4b8f
Leonardo Hernandez <hernandeez00@gmail.com>
2024-11-29 08:40:45 -0300

    minor configs and added dotenv module

--- cypress.config.js
+++ cypress.config.js
@@ -1,20 +1,21 @@
-const { defineConfig } = require('cypress')
+const { defineConfig } = require("cypress");
+const { config } = require('dotenv')
 
 module.exports = defineConfig({
-  watchForFileChanges: false,
-  fixturesFolder: false,
-  viewportWidth: 1200,
-  viewportHeight: 800,
-  video: false,
-  projectId: '9gzopg',
   e2e: {
-    // We've imported your old cypress plugins here.
-    // You may want to clean this up later by importing these.
-    setupNodeEvents(on, config) {
-      return require('./cypress/plugins/index.js')(on, config)
-    },
-    supportFile: false,
     baseUrl: 'http://localhost:3000',
     specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
+    watchForFileChanges: false,
+    viewportWidth: 1200,
+    viewportHeight: 800,
+    fixturesFolder: false,
+    video: false,
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+      return require('./cypress/plugins/index.js')(on, config)
+    },
   },
-})
+  env: {
+    ...process.env
+  },
+});
